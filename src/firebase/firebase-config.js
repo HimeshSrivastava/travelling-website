4402d8ba2fbbcb64559a210897c45e6231b2f23e
HimeshSrivastava <153629881+HimeshSrivastava@users.noreply.github.com>
2025-04-24 13:02:05 +0530

    Firbase credential to run it

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,11 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC4Yt2N3_va6wJTegVdHcapTuo14F4cceI",
   authDomain: "travelling-website-80af2.firebaseapp.com",
@@ -16,6 +10,5 @@ const firebaseConfig = {
   measurementId: "G-DMZ698KFN6"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
